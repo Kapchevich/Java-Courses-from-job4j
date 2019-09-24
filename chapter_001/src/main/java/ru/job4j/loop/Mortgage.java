9b0bba757916e505bf015171a3769380e7593ef2
Kapchevich <kapchevich@gmail.com>
2019-09-24 20:23:01 +0300

    5.7. Ипотека.[#180587]

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,11 +3,18 @@ package ru.job4j.loop;
 public class Mortgage {
     public int year(int amount, int salary, double percent) {
         int year = 0;
-        double accrued = amount * (percent/100);
+        double accrued = amount * (percent / 100);
         double total = amount + accrued;
-        while (total >0) {
+        while (total > 0) {
             total -= salary;
             year++;
-        } return year;
+            if (total > 0) {
+                double accrued2 = total * (percent / 100);
+                double total2 = accrued2 + total;
+                total = total2;
+            }
+        }
+        return year;
     }
-    }
+}
+
