49a7403caf4aeff9e7a96ed22f5323817ce2f6c5
Kapchevich <kapchevich@gmail.com>
2019-09-25 21:53:36 +0300

    5.8. JavaDoc. Code convention[#180583]

--- chapter_001/src/main/java/ru/job4j/calculator/package-info.java
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -1,7 +1,7 @@
-package ru.job4j.calculator;
 /**
  * Package for calculate task.
  * @author Mikhail Litovchenko (kapchevich@gmail.com)
  * @version $Id$
  * @since 0.1
  */
+package ru.job4j.calculator;
