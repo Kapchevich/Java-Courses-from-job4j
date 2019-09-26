cf296a7ebb5996803c79a8c827b5676520385aa2
Kapchevich <kapchevich@gmail.com>
2019-09-26 18:11:27 +0300

    5.6. Простые числа.[#180586]

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,16 +3,21 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
-        for (int a = 2; a <= finish; a++) {
-            count++;
-            for (int b = 2; b < a; b++) {
-                if (a % b == 0) {
-                    count --;
+        for (int i = 2; i <= finish; i++) {
+            boolean prime = true;
+            if (prime) {
+                count++;
+            }
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                  prime = false;
+                  count--;
                     break;
                 }
             }
-        }
-        return count;
+
+        }return count;
+
     }
 }
 
