cc160617c3a4e1dd194557e31fa399e63662ed4f
Kapchevich <kapchevich@gmail.com>
2019-09-30 14:07:44 +0300

    6.7. Выигрышные комбинации в сокобан[#180596]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class MatrixCheck {
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            int a = 0;
+            int b = 0;
+            if (board[i][i] == 'X') {
+                for (int j = 0; j < board.length; j++) {
+                    if (board[i][j] == 'X') {
+                        a++;
+                    } if (a == board.length) {
+                        result = true;
+                    } if (board[j][i] == 'X') {
+                        b++;
+                    } if (b == board.length) {
+                        result = true;
+                    }
+                }
+            }
+        } return result;
+    }
+}
+
+
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.array;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+    @Ignore
+    public class MatrixCheckTest {
+        @Test
+        public void whenDataMonoByTrueThenTrue() {
+            char[][] input = {
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+            };
+            boolean result = MatrixCheck.isWin(input);
+            assertThat(result, is(true));
+        }
+
+        @Test
+        public void whenDataNotMonoByTrueThenFalse() {
+            char[][] input = {
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', 'X', ' ', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+            };
+            boolean result = MatrixCheck.isWin(input);
+            assertThat(result, is(false));
+        }
+
+        @Test
+        public void whenDataMonoByTrueThenTrue2() {
+            char[][] input = {
+                    {' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' '},
+                    {'X', 'X', 'X', 'X', 'X'},
+                    {' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' '},
+            };
+            boolean result = MatrixCheck.isWin(input);
+            assertThat(result, is(true));
+        }
+        @Test
+        public void whenDataMonoByTrueThenTrue8() {
+            char[][] input = {
+                    {' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', 'X', ' ', ' '},
+                    {'X', 'X', ' ', 'X', 'X'},
+                    {' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' '},
+            };
+            boolean result = MatrixCheck.isWin(input);
+            assertThat(result, is(false));
+        }
+    }
