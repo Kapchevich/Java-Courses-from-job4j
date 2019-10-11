9ae60a736175b29f494ff6d2bf3172dda943744b
Kapchevich <kapchevich@gmail.com>
2019-10-11 23:47:11 +0300

    4. Расстояние между точками в трехмерном пространстве.[#180709]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -1,37 +1,21 @@
 package ru.job4j.condition;
-import org.junit.Assert;
 import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class PointTest {
     @Test
-    public void distance() {
-        int in = 0;
-        int in2 = 0;
-        int in3 = 2;
-        int in4 = 0;
-        double expected = 3.0;
-        double out = Point.distance(in, in2, in3, in4);
-        Assert.assertEquals(expected, out, 0.01);
+    public void d3() {
+        Point a = new Point(6, 2, 5);
+        Point b = new Point(4, 5, 8);
+        double result3d = a.distance3d(b);
+        assertThat(result3d, is(4.69041575982343));
     }
     @Test
-    public void distance2() {
-        int in = 1;
-        int in2 = 2;
-        int in3 = 6;
-        int in4 = 18;
-        double expected = 16.76;
-        double out = Point.distance(in, in2, in3, in4);
-        Assert.assertEquals(expected, out, 0.01);
+    public void d2() {
+        Point a = new Point(8,4);
+        Point b = new Point(4, 12);
+        double result = a.distance(b);
+        assertThat(result, is(8.94427190999916));
     }
-    @Test
-    public void distance3() {
-        int in = 6;
-        int in2 = 1;
-        int in3 = 8;
-        int in4 = 12;
-        double expected = 11.18;
-        double out = Point.distance(in, in2, in3, in4);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
 }
