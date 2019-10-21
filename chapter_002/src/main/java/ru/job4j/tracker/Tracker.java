7a09e23fc4d8b7790a4680cd69ddee20f160afb0
Kapchevich <kapchevich@gmail.com>
2019-10-21 16:02:40 +0300

    5. Реализовать класс Tracker[#180729]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -18,48 +18,52 @@ public class Tracker {
         Random rm = new Random();
         return String.valueOf(rm.nextLong() + System.currentTimeMillis());
     }
+
     public boolean replace(String id, Item item) {
         boolean result = false;
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i <= this.position; i++) {
             if (item != null && item.getId().equals(id)) {
                 items[i] = item;
+                item.setId(id);
                 result = true;
                 break;
             }
         }return result;
     }
+
     public Item findById(String id) {
        Item result = null;
-       for (int i = 0; i < items.length; i++) {
+       for (int i = 0; i <= this.position; i++) {
            Item find = items[i];
            if (find != null && find.getId().equals(id)) {
                result = find;
            }
        } return result;
     }
+
     public boolean delete(String id) {
         boolean result = false;
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i <= this.position; i++) {
             if (items[i] != null && items[i].getId().equals(id)) {
                 System.arraycopy(items, i, this.items, i + 1, items.length - 1);
                 result = true;
+                this.position--;
                 break;
             }
         } return result;
     }
-    public Item[] findByName(String key) {
-        Item[] find = new Item[items.length];
-        for (int i = 0; i < items.length; i++) {
-            if (items[i] != null && items[i].getName().equals(key)) {
-                find[i] = items[i];
-                break;
-            }
 
-        } return find;
+    public Item[] findByName(String key) {
+        int count = 0;
+        for (int i = 0; i <= this.position; i++) {
+            if (items[i] != null && items[i].getName().equals(key)) {
+                count++;
+            }
+        } return Arrays.copyOf(items, count);
     }
+
     public Item[] findAll() {
-        System.out.println(Arrays.toString(items));
-        return items;
+        return Arrays.copyOf(items, position);
     }
 }
 
