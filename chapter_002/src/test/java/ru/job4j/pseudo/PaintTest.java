514df46a15b36fa959d6269ed654e98f51892d24
Kapchevich <kapchevich@gmail.com>
2019-11-03 19:26:08 +0300

    t.5. Рефакторинг теста @Before @After[#180688]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,4 +1,6 @@
 package ru.job4j.pseudo;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.stragery.Paint;
 import ru.job4j.stragery.Square;
@@ -6,18 +8,30 @@ import ru.job4j.stragery.Triangle;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
                         new StringBuilder()
                                 .append("O O O O")
@@ -31,27 +45,24 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
+
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(
-                new String(out.toByteArray()),
+                this.out.toString(),
                 is(
-                        new StringBuilder()
-                                .append("  O ")
-                                .append("\n")
-                                .append(" O O ")
-                                .append("\n")
-                                .append("O   O ")
-                                .append(System.lineSeparator())
+                        new StringJoiner(
+                                System.lineSeparator(), "",
+                                System.lineSeparator())
+                                .add("  O ")
+                                .add("\n")
+                                .add(" O O ")
+                                .add("\n")
+                                .add("O   O ")
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
 }
