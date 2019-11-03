a7056724633bb0b6db304a567fd8884209f14ca4
Kapchevich <kapchevich@gmail.com>
2019-11-03 17:54:57 +0300

    t.3. Используя класс StubInput написать тесты проверяющие поведение пользователя[#180692]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,38 +1,17 @@
 package ru.job4j.tracker;
-
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
     }
-    @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {item.getId(), "replaced item"};
-        StartUI.replaceItem(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-    @Test
-    public void whenDeleteItem() {
-       Tracker tracker = new Tracker();
-       Item item = new Item("New Item");
-       tracker.add(item);
-       String[] answers = {item.getId()};
-       StartUI.deleteItem(new StubInput(answers), tracker);
-       Item delete = tracker.findById(item.getId());
-       assertEquals(delete, null);
-    }
+
 }
