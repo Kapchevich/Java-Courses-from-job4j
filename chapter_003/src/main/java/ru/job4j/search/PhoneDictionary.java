cc4afbc8a25f15aaa2fbe7d8f0b977a283072558
Kapchevich <kapchevich@gmail.com>
2019-11-29 19:40:36 +0300

    1. Телефонный справочник на базе ArrayList[#180663]

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,6 @@
 package ru.job4j.search;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class PhoneDictionary {
@@ -10,23 +10,17 @@ public class PhoneDictionary {
         this.persons.add(person);
     }
 
-    /**
-     * Вернуть список всех пользователей, который содержат key в любых полях.
-     *
-     * @param key Ключ поиска.
-     * @return Список подощедщих пользователей.
-     */
     public List<Person> find(String key) {
         List<Person> result = new ArrayList<>();
-        for (int i = 0; i < this.persons.size(); i++) {
-            if (key.contains(persons.get(i).getName())
-                    || key.contains(persons.get(i).getSurname())
-                    || key.contains(persons.get(i).getAddress())
-                    || key.contains(persons.get(i).getPhone())) {
-                result.add(persons.get(i));
+        for (int i = 0; i < persons.size(); i++) {
+            if (key.contains(this.persons.get(i).getName()) ||
+                    key.contains(this.persons.get(i).getSurname()) ||
+                    key.contains(this.persons.get(i).getPhone()) ||
+                    key.contains(this.persons.get(i).getAddress())) {
+                result.add(this.persons.get(i));
             }
-
-        }return result;
+        }
+        return result;
     }
 }
 
