df16189928d69c3f8e834372ea8fc6c6c598e20e
Kapchevich <kapchevich@gmail.com>
2020-01-02 18:57:59 +0300

    Группировка по интересам. [#106993]

--- chapter_004/src/main/java/ru/job4j/lambda/Group.java
+++ chapter_004/src/main/java/ru/job4j/lambda/Group.java
@@ -5,13 +5,7 @@ import java.util.stream.Collectors;
 
 public class Group {
     public  Map<String, Set<String>> sections(List<Student> students) {
-        List<Holder> holderList = new ArrayList<>();
-        for (Student student : students) {
-            for (String unit : student.getUnits()) {
-                holderList.add(new Holder(student.getName(), unit));
-            }
-        }
-        Map<String, Set<String>> map = holderList.stream().collect(Collectors.groupingBy(Holder::getValue,
+        Map<String, Set<String>> map = students.stream().flatMap(s -> s.getUnits().stream().map(i -> new Holder(s.getName(), i))).collect(Collectors.groupingBy(Holder::getValue,
                 Collectors.mapping(Holder::getKey, Collectors.toSet())));
         return map;
     }
